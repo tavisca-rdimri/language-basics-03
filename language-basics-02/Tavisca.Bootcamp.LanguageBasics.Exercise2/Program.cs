f44fffe69a3eea348da9af5f379a430655dacedd
tavisca-rdimri <rdimri@tavisca.in>
2019-06-24 17:05:48 +0530

    Updated code for Problem statement 2

--- language-basics-02/Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ language-basics-02/Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -10,7 +10,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Test(new[] { "23:23:23", "23:23:23" }, new[] { "59 minutes ago", "59 minutes ago" }, "00:22:23");
             Test(new[] { "00:10:10", "00:10:10" }, new[] { "59 minutes ago", "1 hours ago" }, "impossible");
             Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 hours ago" }, "11:59:23");
-            Console.ReadKey(true);
+            //Console.ReadKey(true);
         }
 
         private static void Test(string[] postTimes, string[] showTimes, string expected)
