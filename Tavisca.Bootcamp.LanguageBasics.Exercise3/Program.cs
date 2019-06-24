1324ed54a5f192005e97dcb7452d5eadb21d3070
tavisca-rdimri <rdimri@tavisca.in>
2019-06-24 16:53:29 +0530

    Updated code for Lab Problem 3

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -4,140 +4,142 @@ using System.Collections.Generic;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
-    public static class Program
-    {
-        static void Main(string[] args)
-        {
-            Test(
-                new[] { 3, 4 }, 
-                new[] { 2, 8 }, 
-                new[] { 5, 2 }, 
-                new[] { "P", "p", "C", "c", "F", "f", "T", "t" }, 
-                new[] { 1, 0, 1, 0, 0, 1, 1, 0 });
-            Test(
-                new[] { 3, 4, 1, 5 }, 
-                new[] { 2, 8, 5, 1 }, 
-                new[] { 5, 2, 4, 4 }, 
-                new[] { "tFc", "tF", "Ftc" }, 
-                new[] { 3, 2, 0 });
-            Test(
-                new[] { 18, 86, 76, 0, 34, 30, 95, 12, 21 }, 
-                new[] { 26, 56, 3, 45, 88, 0, 10, 27, 53 }, 
-                new[] { 93, 96, 13, 95, 98, 18, 59, 49, 86 }, 
-                new[] { "f", "Pt", "PT", "fT", "Cp", "C", "t", "", "cCp", "ttp", "PCFt", "P", "pCt", "cP", "Pc" }, 
-                new[] { 2, 6, 6, 2, 4, 4, 5, 0, 5, 5, 6, 6, 3, 5, 6 });
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			Test(
+				new[] { 3, 4 }, 
+				new[] { 2, 8 }, 
+				new[] { 5, 2 }, 
+				new[] { "P", "p", "C", "c", "F", "f", "T", "t" }, 
+				new[] { 1, 0, 1, 0, 0, 1, 1, 0 });
+			Test(
+				new[] { 3, 4, 1, 5 }, 
+				new[] { 2, 8, 5, 1 }, 
+				new[] { 5, 2, 4, 4 }, 
+				new[] { "tFc", "tF", "Ftc" }, 
+				new[] { 3, 2, 0 });
+			Test(
+				new[] { 18, 86, 76, 0, 34, 30, 95, 12, 21 }, 
+				new[] { 26, 56, 3, 45, 88, 0, 10, 27, 53 }, 
+				new[] { 93, 96, 13, 95, 98, 18, 59, 49, 86 }, 
+				new[] { "f", "Pt", "PT", "fT", "Cp", "C", "t", "", "cCp", "ttp", "PCFt", "P", "pCt", "cP", "Pc" }, 
+				new[] { 2, 6, 6, 2, 4, 4, 5, 0, 5, 5, 6, 6, 3, 5, 6 });
             Console.ReadKey(true);
-        }
+		}
 
-        private static void Test(int[] protein, int[] carbs, int[] fat, string[] dietPlans, int[] expected)
-        {
-            var result = SelectMeals(protein, carbs, fat, dietPlans).SequenceEqual(expected) ? "PASS" : "FAIL";
-            Console.WriteLine($"Proteins = [{string.Join(", ", protein)}]");
-            Console.WriteLine($"Carbs = [{string.Join(", ", carbs)}]");
-            Console.WriteLine($"Fats = [{string.Join(", ", fat)}]");
-            Console.WriteLine($"Diet plan = [{string.Join(", ", dietPlans)}]");
-            Console.WriteLine(result);
-        }
+		private static void Test(int[] protein, int[] carbs, int[] fat, string[] dietPlans, int[] expected)
+		{
+			var result = SelectMeals(protein, carbs, fat, dietPlans).SequenceEqual(expected) ? "PASS" : "FAIL";
+			Console.WriteLine($"Proteins = [{string.Join(", ", protein)}]");
+			Console.WriteLine($"Carbs = [{string.Join(", ", carbs)}]");
+			Console.WriteLine($"Fats = [{string.Join(", ", fat)}]");
+			Console.WriteLine($"Diet plan = [{string.Join(", ", dietPlans)}]");
+			Console.WriteLine(result);
+		}
 
-        public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
-        {
-            // Add your code here.
-            int totalMeals = protein.Length;
-            int totalConsumers = dietPlans.Length;
-            int[] menuSelectionIndex = new int[totalConsumers];
+		public static List<int> FindAllIndex(int[] elements, List<int> indexLists, int elem) {
+			
 
-            int[] cal = new int[totalMeals];
-            for(int i=0;i<totalMeals;i++)
-            {
-                cal[i] = 5 * (protein[i] + carbs[i]) + 9 * fat[i];
-            }
-            for(int i=0;i<totalConsumers;i++)
-            {
-                string dietPlanOfIndividual = dietPlans[i];
-                if(dietPlanOfIndividual.Length == 0)
-                {
-                    menuSelectionIndex[i] = 0;
-                }
-                else
-                {
-                    List<int> temporaryListOfIndexes = new List<int>();
-                    for(int j=0;j<totalMeals;j++)
-                    {
-                        temporaryListOfIndexes.Add(j);
-                    }
+			var query = from i in indexLists 
+				where elements[i] == elem 
+				select i; 
+			var indexList = query.ToList();
+			return indexList;
+		}
 
-                    for(int j=0;j<dietPlanOfIndividual.Length;j++)
-                    {
-                        switch(dietPlanOfIndividual[j])
-                        {
-                            case 'P':
-                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes,true,protein);
-                                break;
-                            case 'p':
-                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, false, protein);
-                                break;
-                            case 'C':
-                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, true, carbs);
-                                break;
-                            case 'c':
-                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, false, carbs);
-                                break;
-                            case 'F':
-                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, true, fat);
-                                break;
-                            case 'f':
-                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, false, fat);
-                                break;
-                            case 'T':
-                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, true, cal);
-                                break;
-                            case 't':
-                                temporaryListOfIndexes = recommendMealOfIndividual(temporaryListOfIndexes, false, cal);
-                                break;
-                        }
-                    }
-                    menuSelectionIndex[i] = temporaryListOfIndexes[0];
-                }
-            }
-            return menuSelectionIndex;
-            throw new NotImplementedException();
-        }
+		public static int GetMax(int[] elements, List<int> indexList) {
 
-        public static List<int> recommendMealOfIndividual(List<int> tempIndexes, bool isUpperCase, int[] ingredient)
-        {
-            int keyToBeOptimized = ingredient[tempIndexes[0]];
-            if(tempIndexes.Count > 0 )
-            {
-                if(isUpperCase == true)
-                {
-                    for(int i=1;i<tempIndexes.Count;i++)
-                    {
-                        if(keyToBeOptimized < ingredient[tempIndexes[i]])
-                        {
-                            keyToBeOptimized = ingredient[tempIndexes[i]];
-                        }
-                    }
-                }
-                else
-                {
-                    for (int i = 1; i < tempIndexes.Count; i++)
-                    {
-                        if (keyToBeOptimized > ingredient[tempIndexes[i]])
-                        {
-                            keyToBeOptimized = ingredient[tempIndexes[i]];
-                        }
-                    }
-                }
-            }
+			int max = elements[indexList[0]];
 
-            List<int> newIndexes = new List<int>();
-            for(int i=0;i<tempIndexes.Count;i++)
-            {
-                if(ingredient[tempIndexes[i]] == keyToBeOptimized) {
-                    newIndexes.Add(tempIndexes[i]);
-                }
-            }
-            return newIndexes;
-        }
-    }
-}
+			for (int i = 1; i < indexList.Count; i++) {
+				if (elements[indexList[i]] > max)
+				max = elements[indexList[i]];
+			}
+
+			return max;
+		}
+		public static int GetMin(int[] elements, List<int> indexList) {
+			
+			int min = elements[indexList[0]];
+
+			for (int i = 1; i < indexList.Count; i++) {
+				if (elements[indexList[i]] < min) 
+				min = elements[indexList[i]];
+			}
+
+			return min;
+		}
+
+		public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
+		{  
+
+			 int[] cal = new int[protein.Length];
+			 int[] item = new int[dietPlans.Length];
+
+			 for (int i = 0; i < protein.Length; i++) {
+			 	cal[i] = (protein[i] + carbs[i]) * 5 + fat[i] * 9;
+			 }
+
+			 for (int i = 0; i < dietPlans.Length; i++) {
+			 	string dietPlan = dietPlans[i];
+			 	List<int> indexList = new List<int>();
+			 	int max, min;
+
+			 	if (dietPlan.Length == 0) {
+			 		item[i] = 0;
+			 	}
+
+			 	indexList=Enumerable.Range(0, protein.Length).ToList();
+
+			 	foreach (char ch in dietPlan) {
+
+			 		switch(ch) {
+
+			 			case 'p':
+			 			min = GetMin(protein, indexList);
+			 			indexList = FindAllIndex(protein, indexList, min);
+			 			break;
+			 			case 'P':
+			 			max = GetMax(protein, indexList);
+			 			indexList = FindAllIndex(protein, indexList, max);
+			 			break;
+
+			 			case 'c':
+			 			min = GetMin(carbs, indexList);
+			 			indexList = FindAllIndex(carbs, indexList, min);
+			 			break;
+			 			case 'C':
+			 			max = GetMax(carbs, indexList);
+			 			indexList = FindAllIndex(carbs, indexList, max);
+			 			break;
+
+			 			case 'f':
+			 			min = GetMin(fat, indexList);
+			 			indexList = FindAllIndex(fat, indexList, min);
+			 			break;
+			 			case 'F':
+			 			max = GetMax(fat, indexList);
+			 			indexList = FindAllIndex(fat, indexList, max);
+			 			break;
+
+			 			case 't':
+			 			min = GetMin(cal, indexList);
+			 			indexList = FindAllIndex(cal, indexList, min);
+			 			break;
+			 			case 'T':
+			 			max = GetMax(cal, indexList);
+			 			indexList = FindAllIndex(cal, indexList, max);
+			 			break;
+
+			 		}
+			 	}
+
+			 	item[i] = indexList[0];
+			 }
+
+			 return item;
+			}
+		}
+	}
